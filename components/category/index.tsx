553424d32974cfc49a873462780ff43e3d54ec55
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2023-05-15 17:36:06 +0900

    mainpage 카테고리 추가

--- components/category/index.tsx
+++ components/category/index.tsx
@@ -1,19 +1,41 @@
 import { Container, IconWrapper,ItemContainer,ItemTitle, ListContainer, Title } from "./category.style"
-import Continue from "@/public/img/category/continue.png";
-import SelfImprovement from "@/public/img/category/selfImprovement.png";
-import { StaticImageData } from "next/image";
+import continueRead from "@/public/img/category/continue.png";
+import selfImprovement from "@/public/img/category/selfImprovement.png";
+import investment from "@/public/img/category/investment.png";
+import economy from "@/public/img/category/economy.png";
+import humanities from "@/public/img/category/humanities.png";
+import marketing from "@/public/img/category/marketing.png";
 import Image from "next/image";
-import { useSelector } from 'react-redux';
 export const Category = () =>{
   const itemList = [
     {
       title : '이어서보기',
-      icon : Continue,
+      icon : continueRead,
       color : "yellow"
     },
     {
       title : "자기계발",
-      icon : SelfImprovement,
+      icon : selfImprovement,
+      color : "gray"
+    },
+    {
+      title : '금융/투자',
+      icon : investment,
+      color : "gray"
+    },
+    {
+      title : "경제/경영",
+      icon : economy,
+      color : "gray"
+    },
+    {
+      title : "인문",
+      icon : humanities,
+      color : "gray"
+    },
+    {
+      title : "기획/마케팅",
+      icon : marketing,
       color : "gray"
     }
   ];
