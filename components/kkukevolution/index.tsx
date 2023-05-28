c12db49eaf4ef266877e8d387a324d49ebd7d875
MinJae00 <exceedmin@gmail.com>
2023-05-28 21:46:14 +0900

    책꽂이 가운데 정렬

---
+++ components/kkukevolution/index.tsx
@@ -0,0 +1,20 @@
+
+import {Container} from "./kkukevolution.style"
+import Image from 'next/image';
+import UltraProfit from "@/public/img/book/ultra-profit.png"
+import babykkuk from "@/public/img/library/character/babykkuck.png"
+import secondkkuk from "@/public/img/library/character/2ndkkuk.png"
+import thirdkkuk from "@/public/img/library/character/3ndkkuk.png"
+import fourthkkuk from "@/public/img/library/character/4ndkkuk.png"
+
+export const Kkukevolution = () => {
+    return (
+
+        
+        <Container>
+            
+
+        </Container>
+
+    )
+}
