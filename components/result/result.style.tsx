a4b88be04bb1836bbd67ce505f31622003676822
MinJae00 <exceedmin@gmail.com>
2023-05-29 01:18:06 +0900

    검색결과 추가

--- components/result/result.style.tsx
+++ components/result/result.style.tsx
@@ -3,16 +3,32 @@ import styled from "styled-components";
 
 
 export const DetailContainer = styled.div`
-    width : 170px;
-    margin-left : 10px;
-    box-shadow : 0 6px 6px -5px black;
+    margin-left : -6px;
+    margin-top : 5px;
+    display : flex;
+    flex-wrap: wrap;
+    padding-top : 100%;
 `
 
 export const ThumbContainer = styled.div `
 `
 
+export const BContainer = styled.div `
+width : 170px;
+margin-left : 10px;
+box-shadow : 0 6px 6px -5px black;
+
+`
+export const AContainer = styled.div `
+    display : flex;
+    flex-direction : column;
+`
+
+
+
 export const BookInfoContainer = styled.div`
-    flex-wrap: nowrap;
+    display : flex;
+    flex-direction : row;
     justify-content: space-between;
     align-items: center;
 `
@@ -31,12 +47,17 @@ export const Writer = styled.div `
     margin-top : 5px;
     margin-left : 5px;
     font-size : 8px;
+    .write-style{
+        color : #656565;
+    }
   
 `
 export const Timer = styled.div`
     margin-top : 5px;
-
     margin-left : 85px;
     font-size : 10px;
+    .write-style{
+        color : #656565;
+    }
 
 `
