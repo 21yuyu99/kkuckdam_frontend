9688a9fa20de08a6df1427784b72620e208f8fb7
oncsr <khjice1004@gmail.com>
2023-05-29 01:03:08 +0900

    나만의 서재 아이콘 클릭 구현

--- components/library/kkuck/index.tsx
+++ components/library/kkuck/index.tsx
@@ -1,8 +1,22 @@
-import { Container, Title, KkukkkukContainer, TextContainer, InformText, TopContainer, CharacterWrapper, InformIconWrapper, MainText } from "./kkuk.style"
+import { Container, Title, KkukkkukContainer, TextContainer, InformText, TopContainer, CharacterWrapper, InformIconWrapper, MainText, Kkukbanner, TempContainer, BabyContainer, KkukContainer, AContainer, BContainer, LineContainer } from "./kkuk.style"
 import Image from "next/image";
 import babyKkuck from "@/public/img/library/character/babyKkuck.png";
 import informIcon from "@/public/img/library/informicon.png";
+import xbtn from "@/public/img/library/btn/x.png"
+import babykkuk from "@/public/img/library/character/babykkuck.png"
+import secondkkuk from "@/public/img/library/character/2ndkkuk.png"
+import thirdkkuk from "@/public/img/library/character/3ndkkuk.png"
+import fourthkkuk from "@/public/img/library/character/4ndkkuk.png"
+import { useState } from 'react';
+
 export const KkukBanner = () => {
+    const [isImageClicked, setImageClicked] = useState(false);
+    const handleClick = () => {
+        setImageClicked(true);
+    }
+    const handleClick2 = () => {
+        setImageClicked(false);
+    }
     return(
         <Container>
             <Title>희선 님의 서재</Title>
@@ -16,12 +30,65 @@ export const KkukBanner = () => {
                         <MainText underline={true}>아기 꾹꾹이</MainText>
                         <MainText underline={false}>에요</MainText>
                         <InformIconWrapper>
-                            <Image src={informIcon} alt="꾹꾹이 설명서" width={20} height={20}/>
+                            <Image src={informIcon} onClick={handleClick} alt="꾹꾹이 설명서" width={20} height={20}/>
                         </InformIconWrapper>
                     </TopContainer>
                     <InformText><br></br>다독으로 꾹꾹이를 키워보세요</InformText>
                 </TextContainer>
             </KkukkkukContainer>
+            {isImageClicked?(
+                <TempContainer>
+                    <Kkukbanner>
+                        <Image src = {xbtn} onClick={handleClick2} width = {24} alt = "x버튼"/>
+                        <span className = "text-style">책을 많이 읽을수록 꾹꾹이가 진화해요</span>
+                    </Kkukbanner>
+                    <KkukContainer>
+                        <BabyContainer>
+                            <span className = "kkukname">아기 꾹꾹이</span>
+                            <AContainer>
+                                <Image src = {babykkuk} width = {55} alt = "애기꾹꾹이"/>
+                                <BContainer>
+                                <span className = "explain">태어난지 얼마 안된 꾹꾹이에요!</span>
+                                <span className = "explain">관심과 사랑이 필요해요!</span>
+                                </BContainer>
+                            </AContainer>
+                        </BabyContainer>
+                        <LineContainer/>
+                        <BabyContainer>
+                            <span className = "kkukname">유치원생 꾹꾹이</span>
+                            <AContainer>
+                                <Image src = {secondkkuk} width = {55} alt = "유치원생 꾹꾹이"/>
+                                <BContainer>
+                                <span className = "explain">자라나는 꾹꾹이에요</span>
+                                <span className = "explain">꾸준한 독서로 꾹꾹이를 키워보세요</span>
+                                </BContainer>
+                            </AContainer>
+                        </BabyContainer>
+                        <LineContainer/>
+                        <BabyContainer>
+                            <span className = "kkukname">질풍노도의 꾹꾹이</span>
+                            <AContainer>
+                                <Image src = {thirdkkuk} width = {55} alt = "질풍노도의 꾹꾹이"/>
+                                <BContainer>
+                                <span className = "explain">중2병이 심한 꾹꾹이에요</span>
+                                <span className = "explain">독서를 통해 힘든 시기를 이겨내봐요</span>
+                                </BContainer>
+                            </AContainer>
+                        </BabyContainer>
+                        <LineContainer/>
+                        <BabyContainer>
+                            <span className = "kkukname">어른 꾹꾹이</span>
+                            <AContainer>
+                                <Image src = {fourthkkuk} width = {55} alt = "어른 꾹꾹이"/>
+                                <BContainer>
+                                <span className = "explain">독서가 습관이 된 당신!</span>
+                                <span className = "explain">잘하고 있어요 저와 꾸준히 독서해요</span>
+                                </BContainer>
+                            </AContainer>
+                        </BabyContainer>
+                    </KkukContainer>
+                </TempContainer>
+            ):(<div></div>)}
         </Container>
     )
 }
--- components/library/kkuck/kkuk.style.ts
+++ components/library/kkuck/kkuk.style.ts
@@ -1,14 +1,8 @@
 import styled from "styled-components"
-// 2023.05.16 00:42
-/*
-  컴포넌트는 기본적으로 위에서 아래로 배치됨
-  https://studiomeal.com/archives/197
 
-  flex라는 걸 좀 더 알아보거나
-  각 컴포넌트를 담는 컨테이너를 따로 만들어서 수동 배치
-*/
 export const Container = styled.div`
 `
+
 export const Title = styled.h2`
   font-size:1.3em;
   margin:15px auto;
@@ -82,4 +76,92 @@ padding-left:5px;
     width:50%;
     height:50%;
 }
+`
+
+// 아래로는 꾹꾹이 진화 화면에 관한 것들
+
+export const TempContainer = styled.div`
+display: flex;
+justify-content: center;
+position: absolute;
+z-index:1;
+
+left:0;
+right:0;
+margin:0 auto;
+
+`
+
+
+export const Kkukbanner = styled.div`
+    position: absolute;
+    width : 340px;
+    height : 580px;
+    margin-top:-40px;
+    display : flex;
+    border-radius : 13px 13px 0 0;
+    background-color : #FDF0D4;
+    border-style : solid;
+    border-color : #33314A;
+    align-items : center;
+    z-index : 1;
+    
+    > img{
+        position : absolute;
+        top : 10px;
+        right : 10px;
+    }
+    .text-style{
+        position : absolute;
+        top : 35px;
+        font-size : 16px;
+        width : 100%;
+        text-align : center;
+    }
+`
+export const KkukContainer = styled.div`
+    display : flex;
+    flex-direction : column;
+
+`
+
+export const BabyContainer = styled.div`
+
+    display : flex;
+    flex-direction : column;
+    z-index : 1;
+    margin-top : 35px;
+    margin-left : 10px;
+    .kkukname{
+        margin-bottom : 20px;
+        color : #D46863;
+    }
+
+`
+export const AContainer = styled.div`
+
+    display : flex;
+    flex-direction : row;
+    margin-left : 20px;
+
+`
+export const BContainer = styled.div`
+    display : flex;
+    flex-direction : column;
+    margin-left : 20px;
+    margin-top : 10px;
+    font-size : 13px;
+    color : #33314A;
+`
+
+export const LineContainer = styled.div`   
+    display : flex;
+    border-style : solid;
+    border-color : #999999;
+    border-radius : 13px;
+    margin-top : 15px;
+    margin-bottom : -20px;
+    flex: 1; 
+    width: 100%;
+    z-index : 1;
 `
