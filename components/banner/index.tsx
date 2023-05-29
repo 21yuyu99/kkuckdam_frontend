95796f109e42d466253564b672519c92523e5624
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2023-05-29 19:50:10 +0900

    메인 배너상태관리 추가

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -1,54 +1,106 @@
 import Image, { StaticImageData } from "next/image";
-import { BannerWrapper, Container, PaginationContainer, PointCircle, UnPointCircle } from "./mainBanner.style";
+import { BannerWrapper, Container, PaginationContainer, Circle } from "./mainBanner.style";
 import canRead from "@/public/img/banner/canRead.png";
 import bookAd from "@/public/img/banner/bookAd.png";
 import paperBook from "@/public/img/banner/paperBook.png";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 export const MainBanner = () =>{
-  const bannerList = [
+  interface bannerType{
+    img :  StaticImageData,
+    explain : string,
+    point : boolean
+  }
+  const bannerList:bannerType[] = [
     {
       img : canRead,
       explain : "누구나 완독할 수 있어\n꾹담이 있으니까",
+      point : true,
     },
     {
       img : bookAd,
-      explain : "아무리 힘든 일이라도 누구나\n 하루동안은 할 수 있다."
+      explain : "아무리 힘든 일이라도 누구나\n 하루동안은 할 수 있다.",
+      point : false
     },
     {
       img : paperBook,
-      explain : "종이책 구독 서비스 시작"
+      explain : "종이책 구독 서비스 시작",
+      point : false
     }
   ];
-const [selectedBanner,setBanner] = useState(0);
-let count = 1;
-if(typeof(document)!="undefined"){
-}
-// ul.style.width = `${outer.clientWidth * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
-  const onClickHandler = ()=>{
-    // if(typeof(document)!="undefined"){
-    //  setBanner((selectedBanner + 1)% 3);
-    // }
-    if(typeof(document)!="undefined"){
-      let deviceWidth = document.body.clientWidth;
-      const ul = document.querySelector(`#ul`);
-      const li = document.querySelector(`#li${selectedBanner}`);
-      if(ul instanceof HTMLElement){
-        ul.style.marginLeft = `-${deviceWidth*count-15*count}px`;
-        count++;
-      }    
-    }
+  const [banners,setBanner] = useState(bannerList);
+  const [count,setCount] = useState(1);
+  interface IUseInterval {
+    (callback: () => void, interval: number): void;
   }
+  const useInterval: IUseInterval = (callback, interval) => {
+    const savedCallback = useRef<(() => void) | null>(null);
+    
+    useEffect(() => {
+      savedCallback.current = callback;
+    });
+    useEffect(() => {
+      function tick() {
+        if (savedCallback.current) {
+          savedCallback.current();
+        }
+      }
+  
+      let id = setInterval(tick, interval);
+      return () => clearInterval(id);
+    }, [interval]);
+  };
+  useInterval(
+    () => 
+    {
+      if(typeof(document)!="undefined"){
+        let deviceWidth = document.body.clientWidth;
+        const ul = document.querySelector(`#ul`);
+        const li0 = document.querySelector(`#li0`);
+        const li2 = document.querySelector(`#li2`);
+        if(ul instanceof HTMLElement){
+          setBanner(
+            banners.map((banner,index)=>
+              index == count?{...banner,point : true}:{...banner,point : false}
+              )
+          );
+          if(count == 3){
+            if(li0 instanceof HTMLElement && li2 instanceof HTMLElement){
+                const newNode = li0.cloneNode(true);
+                li2.after(newNode);
+                ul.style.marginLeft = `-${deviceWidth*count-15*count}px`;
+                setTimeout(()=>{
+                  ul.style.transition = 'none';
+                  ul.style.marginLeft = `0px`;
+                },500);
+                setCount(1);
+                ul.style.transition = '.3s ease-out';
+                setBanner(
+                  banners.map((banner,index)=>
+                    index == 0?{...banner,point : true}:{...banner,point : false}
+                    )
+                );
+            }              
+          }
+          else{
+            ul.style.transition = '.3s ease-out';
+            ul.style.marginLeft = `-${deviceWidth*count-15*count}px`;
+            setCount(count+1);
+          }
+        }    
+      }
+    }
+    ,3000);
   return(
-    <Container id="container" onClick={()=>onClickHandler()}>
+    <Container id="container">
       <BannerWrapper id="ul">
-        <Image className = "li" id={`li${selectedBanner}`} src={bannerList[selectedBanner].img} alt={bannerList[selectedBanner].explain}/>
-        <Image className = "li" id={`li${selectedBanner+1}`} src={bannerList[(selectedBanner+1)%3].img} alt={bannerList[(selectedBanner+1)%3].explain}/>
-        <Image className = "li" id={`li${selectedBanner+2}`} src={bannerList[(selectedBanner+2)%3].img} alt={bannerList[(selectedBanner+1)%3].explain}/>
+        <Image className = "li" id={`li0`} src={bannerList[0].img} alt={bannerList[0].explain}/>
+        <Image className = "li" id={`li1`} src={bannerList[1].img} alt={bannerList[1].explain}/>
+        <Image className = "li" id={`li2`} src={bannerList[2].img} alt={bannerList[2].explain}/>
       </BannerWrapper>
       <PaginationContainer>
-        <PointCircle/>
-        <UnPointCircle/>
-        <UnPointCircle/>
+        <Circle point={banners[0].point}/>
+        <Circle point={banners[1].point}/>
+        <Circle point={banners[2].point}/>
       </PaginationContainer>
     </Container>
   )
--- components/banner/mainBanner.style.ts
+++ components/banner/mainBanner.style.ts
@@ -3,10 +3,12 @@ export const Container = styled.div`
   padding-left:15px;
   margin:auto;
   position:relative;
+  overflow:hidden;
   div>img{
     width:100%;
     height:100%;
     padding-right:15px;
+    border-radius: 20px 0px 0px 0px;
   }
 `
 export const BannerWrapper = styled.div`
@@ -28,9 +30,6 @@ bottom:8%;
   margin:0 5px;
 }
 `
-export const PointCircle = styled.div`
-background-color:rgba(60, 60, 60, 0.87);
-`
-export const UnPointCircle = styled.div`
-background-color: #D9D9D9;
+export const Circle = styled.div<{point:boolean}>`
+${props=>props.point?`background-color:rgba(60, 60, 60, 0.87);`:`background-color: #D9D9D9;`}
 `
