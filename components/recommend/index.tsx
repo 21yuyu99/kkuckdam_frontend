5a0391d3c86018d91ee7c7c908f094aa5ba520f1
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2023-05-31 17:31:14 +0900

    이게 현실적으로 구현이 가능한가요?

--- components/recommend/index.tsx
+++ components/recommend/index.tsx
@@ -21,28 +21,65 @@ export const Recommend = () =>{
   )
 }
 export const ForYouBook = () =>{
-const [bookNum,setBookNum] = useState(3);
-const [count,setCount] = useState(1);
+const bookList = [
+  {
+    img : SelfManagement,
+    alt : "자기관리론",
+   },{
+    img : UltraProfit,
+    alt : "초수익 성장주 투자",
+   },{
+    img : BearMarket,
+    alt : "베어마켓",
+   },{
+    img : NatureOfMoney,
+    alt : "돈의 속성",
+   },
+   {
+    img :RichFriend,
+    alt : "나의 돈 많은 고등학교 친구",
+   }
+]
+let bookNum = 7;
+let count = 0;
+let lock = false;
 useEffect(() => {
   // window.addEventListener("scroll", 
   // ()=>console.log(scrollY));
-    let prevBook = document.getElementById(`book${bookNum-1}`);
-    let book = document.getElementById(`book${bookNum}`);
-    let nextBook = document.getElementById(`book${bookNum+1}`);
-    // let top1 = text?.getBoundingClientRect().left;
+      // let top1 = text?.getBoundingClientRect().left;
     // console.log(top1);
-    let bookContainer = document.getElementById("livebook");
-    bookContainer?.scrollTo((836- screen.availWidth)/2,0);
-    let currentScroll = (836- screen.availWidth)/2;
-    bookContainer?.addEventListener("scroll",()=>{
-      console.log(currentScroll);
+
+  let bookContainer = document.getElementById("livebook");
+  bookContainer?.scrollTo((2360- screen.availWidth)/2,0);
+//book 5~9
+  let currentScroll = (836- screen.availWidth)/2;
+  let prevBook: HTMLElement | null;
+    // if(bookNum == 1)
+    //   prevBook = document.getElementById(`book5`);
+    // else
+      prevBook = document.getElementById(`book${bookNum-1}`);
+    let book = document.getElementById(`book${bookNum}`);
+  let nextBook: HTMLElement | null;
+    // if(bookNum == 5)
+    //     nextBook = document.getElementById(`book1`);
+    //else
+      nextBook = document.getElementById(`book${bookNum+1}`);
+        //console.log(currentScroll);
+      bookContainer?.addEventListener("scroll",()=>{
       if(bookContainer instanceof HTMLElement && book instanceof HTMLElement && nextBook instanceof HTMLElement && prevBook instanceof HTMLElement){
-        if(bookContainer.scrollLeft-currentScroll>=140){
+        let changeScroll = bookContainer.scrollLeft-currentScroll; //762;
+        console.log(changeScroll);
+       // console.log(bookNum);
+       //993
+        if(changeScroll>=(830 + 68*count) && changeScroll < 960){ //830 - 762 = 68
           nextBook.style.transition = "1s ease-out";
-          book.style.width = "143px";book.style.height="220px";
+          book.style.width = "135px";book.style.height="215px";
           nextBook.style.width = "170px";nextBook.style.height="260px";
-          currentScroll = currentScroll+140;
+          //currentScroll = currentScroll+860;
+          bookNum++;
+          count++;
         }
+        console.log(count);
         // else if(bookContainer.scrollLeft-currentScroll<=140){
         //   prevBook.style.transition = "1s ease-out";
         //   book.style.width = "143px";book.style.height="220px";
@@ -67,11 +104,32 @@ const toDetailPage = (id:number)=>{
     <>
     <Title>희선님에게 엄선된 라이브북</Title>
     <ListContainer id = "livebook">
-      <Image id = "book1" src={SelfManagement} alt = "자기관리론" width={143} height={220} onClick={()=>toDetailPage(1)}/>
-      <Image id = "book2" src={UltraProfit} alt = "초수익 성장주 투자" width={143} height={220}/>
+      {
+        bookList.map((book,idx)=>{
+          return(
+            <Image key = {idx} id = {`book${idx}`} src={book.img} alt = {book.alt} width={135} height={215} onClick={()=>toDetailPage(idx+1)}/>
+           )
+        })
+      }
+      {
+        bookList.map((book,idx)=>{
+          return(
+            idx == 2?<><Image key = {idx} id = {`book${idx+5}`} src={book.img} alt = {book.alt} width={170} height={260} onClick={()=>toDetailPage(idx-4)}/></>:<><Image key = {idx} id = {`book${idx+5}`} src={book.img} alt = {book.alt} width={135} height={215} onClick={()=>toDetailPage(idx-4)}/></>
+           )
+          })
+      }
+      {
+        bookList.map((book,idx)=>{
+         return(
+          <Image key = {idx+10} id = {`book${idx+10}`} src={book.img} alt = {book.alt} width={135} height={215} onClick={()=>toDetailPage(idx-8)}/>
+         )
+        })
+      }
+      {/* <Image id = "book1" src={SelfManagement} alt = "자기관리론" width={135} height={215} onClick={()=>toDetailPage(1)}/>
+      <Image id = "book2" src={UltraProfit} alt = "초수익 성장주 투자" width={135} height={215}/>
       <Image id = "book3" src={BearMarket} alt = "베어마켓" width={170} height={260}/>  
-      <Image id = "book4" src={NatureOfMoney} alt = "돈의 속성" width={143} height={220}/>
-      <Image id = "book5" src={RichFriend} alt = "나의 돈 많은 고등학교 친구" width={143} height={220}/>
+      <Image id = "book4" src={NatureOfMoney} alt = "돈의 속성" width={135} height={210}/>
+      <Image id = "book5" src={RichFriend} alt = "나의 돈 많은 고등학교 친구" width={135} height={214}/> */}
     </ListContainer>
     </>
   )
